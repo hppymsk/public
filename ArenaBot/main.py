8c5cef02d4b455b00eccd01e5c9378bb80b856d7
Josh <albertson.josh@gmail.com>
2021-06-22 12:23:41 -0700

    Bug fixes, improved exception handling

--- ArenaBot/main.py
+++ ArenaBot/main.py
@@ -18,6 +18,8 @@ with open('5v5.json', errors='ignore') as read_file:
         fives = json.load(read_file)
 
 def cutoff(bracket):
+  if bracket not in ['2', '2v2', '2s', 'twos', '3', '3v3', '3s', 'threes', '5', '5v5', '5s', 'fives']:
+    bracket = '3'
 
   if bracket in ['2', '2v2', '2s', 'twos']:
     rank1 = 'Infernal Gladiator: ' + str(rewards['rewards'][0]['rating_cutoff']) + '\n'
@@ -45,32 +47,36 @@ def cutoff(bracket):
     challenger = 'Challenger: ' + str(rewards['rewards'][14]['rating_cutoff'])
     cutoffs  = '5v5 Cutoffs:' + '\n' + rank1 + gladiator + duelist + rival + challenger
     return cutoffs
-  else:
-    cutoffs = 'Enter a bracket, for example: !cutoff 2'
-    return cutoffs
 
 def top(bracket):
   top = ''
+  if bracket not in ['2', '2v2', '2s', 'twos', '3', '3v3', '3s', 'threes', '5', '5v5', '5s', 'fives']:
+    bracket = '3'
+
   if bracket in ['2', '2v2', '2s', 'twos']:
     for i in range(0,10):
       top = top + '\n' + str(i+1) + ' - ' + str(twos['entries'][i]['team']['name']) + ' : ' + str(twos['entries'][i]['rating'])
     return 'Top 10 2v2 Teams: ' + top
+
   if bracket in ['3', '3v3', '3s', 'threes']:
     for i in range(0,10):
       top = top + '\n' + str(i+1) + ' - ' + str(threes['entries'][i]['team']['name']) + ' : ' + str(threes['entries'][i]['rating'])
     return 'Top 10 3v3 Teams: ' + top
+
   if bracket in ['5', '5v5', '5s', 'fives']:
     for i in range(0,10):
       top = top + '\n' + str(i+1) + ' - ' + str(fives['entries'][i]['team']['name']) + ' : ' + str(fives['entries'][i]['rating'])
     return 'Top 10 5v5 Teams: ' + top
-  else:
-    return top
 
 def teams(player):
+  if player is None:
+    return None
+  
   player = player.capitalize()
   twosteam = 'No 2v2 Team Found for ' + player
   threesteam = 'No 3v3 Team Found for ' + player
   fivesteam = 'No 5v5 Team Found for ' + player
+
   for x in range(len(twos['entries'])):
       if 'members' not in twos['entries'][x]['team']:
           continue
@@ -99,23 +105,24 @@ def title(player):
   return 'NYI'
 
 def roll(rollrange):
-  rollrange = int(rollrange)
-  if rollrange is not None:
+  try:
+    rollrange = int(rollrange)
     return random.randint(1, rollrange)
-  else:
+  except:
     return random.randint(1, 100)
 
 def eightball():
-  number = random.randint(0, 19)
   outcome = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.',
   'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Don\'t count on it.',
   'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
-  return outcome[number]
+  return outcome[random.randint(0, 19)]
 
 def points(rating):
-  if rating is not type(int):
-    return None
-  rating = int(rating)
+  try:
+    rating = int(rating)
+  except:
+    rating = 1500
+
   if rating > 1500:
       points = (1511.26/(1+1639.28*2.71828**(-0.00412*rating)))
   if rating <= 1500:
@@ -123,7 +130,7 @@ def points(rating):
   twospoints = points*0.76
   threespoints = points*0.88
 
-  return('\n' + '2v2: ' + str(round(twospoints)) + '\n' + '3v3: ' + str(round(threespoints)) + '\n' + '5v5: ' + str(round(points)))
+  return('_ _' + '\n' + '2v2: ' + str(round(twospoints)) + '\n' + '3v3: ' + str(round(threespoints)) + '\n' + '5v5: ' + str(round(points)))
 
 @client.event
 async def on_ready():
@@ -136,7 +143,7 @@ async def on_message(message):
 
     msg = message.content
 
-    if msg.startswith('!commands') or msg.startswith ('!help'):
+    if msg.startswith('!command') or msg.startswith ('!help'):
       await message.channel.send(
         'Current Commands:' + '\n' 
         + '!cutoff <2, 3, 5> - Shows title cutoffs for selected bracket' + '\n'
@@ -146,17 +153,45 @@ async def on_message(message):
         + '!roll <number> - Rolls from 1-number, default value of 100' + '\n'
         + '!8ball - Ask the magic 8 ball a question to reveal your future' + '\n'
       )
-    if msg.startswith('!cutoff'):
-        bracket = msg.split("!cutoff ",1)[1]
-        await message.channel.send(cutoff(bracket))
+    if msg.startswith('!cut'):
+        try:
+          bracket = msg.split("!cutoff ",1)[1]
+        except:
+          bracket = '3'
+        finally:
+          msgcutoff = cutoff(bracket)
+          if msgcutoff is not None:
+            await message.channel.send(msgcutoff)
 
     if msg.startswith('!top'):
-      bracket = msg.split('!top ',1)[1]
-      await message.channel.send(top(bracket))
+      try:
+        bracket = msg.split('!top ',1)[1]
+      except:
+        bracket = '3'
+      finally:
+        msgtop = top(bracket)
+        if msgtop is not None: 
+          await message.channel.send(msgtop)
     
-    if msg.startswith('!teams'):
-      player = msg.split('!teams ',1)[1]
-      await message.channel.send(teams(player))
+    if msg.startswith('!team') or msg.startswith('!rating'):
+      try:
+        player = msg.split('!teams ',1)[1]
+      except:
+        await message.channel.send('Specify a player: !teams Joe')
+      else:
+        msgteams = teams(player)
+        if msgteams is not None:
+          await message.channel.send(msgteams)
+    
+    if msg.startswith('!point'):
+      try:
+        rating = msg.split('!points ',1)[1]
+      except:
+        rating = 1500
+      finally:
+        msgpoints = points(rating)
+        if points(rating) is not None:
+          await message.channel.send(msgpoints)
 
     if msg.startswith('!roll'):
       try:
@@ -164,22 +199,18 @@ async def on_message(message):
       except:
         rollrange = 100
       finally:
-        await message.channel.send(roll(rollrange))
-    
-    if msg.startswith('!hasquinnkilledsireyet'):
-      await message.channel.send('No.')
-    
-    if msg.startswith('!mcdonalds'):
-      await message.channel.send('Just the best, honestly.')
-      await message.add_reaction('❤')
+        msgroll = roll(rollrange)
+        await message.channel.send(msgroll)
 
     if msg.startswith('!8ball'):
       await message.channel.send(eightball())
 
-    if msg.startswith('!points'):
-      rating = msg.split('!points ',1)[1]
-      if points(rating) is not None:
-        await message.channel.send(points(rating))
+    if msg.startswith('!hasquinn'):
+      await message.channel.send('No.')
+    
+    if msg.startswith('!mcd'):
+      await message.channel.send('Just the best, honestly.')
+      await message.add_reaction('❤')
     
 
 client.run(os.environ['DISCORD-API-TOKEN'])
